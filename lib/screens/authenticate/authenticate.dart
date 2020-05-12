55414d205b1127754d48dd525b7495e688169997
kharioki <antolisah@gmail.com>
2020-05-12 15:11:04 +0300

    added signin anonymously

--- lib/screens/authenticate/authenticate.dart
+++ lib/screens/authenticate/authenticate.dart
@@ -1,3 +1,4 @@
+import 'package:brew_crew/screens/authenticate/sign_in.dart';
 import 'package:flutter/material.dart';
 
 class Authenticate extends StatefulWidget {
@@ -8,8 +9,6 @@ class Authenticate extends StatefulWidget {
 class _AuthenticateState extends State<Authenticate> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('authenticate'),
-    );
+    return Container(child: SignIn());
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,5 +1,5 @@
 import 'package:brew_crew/screens/authenticate/authenticate.dart';
-import 'package:brew_crew/screens/home/home.dart';
+// import 'package:brew_crew/screens/home/home.dart';
 import 'package:flutter/material.dart';
 
 class Wrapper extends StatelessWidget {
